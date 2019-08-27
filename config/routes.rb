e3069872c70c91263d36f05db5ef9af61859cb1f
Brian Zanti <brianzanti@gmail.com>
2019-08-27 10:50:56 -0600

    adds restful controller exercises

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/dogs', to: 'dogs#index'
-  get '/dogs/:id', to: 'dogs#show'
   get '/dogs/new', to: 'dogs#new'
+  get '/dogs/:id', to: 'dogs#show'
   post '/dogs', to: 'dogs#create'
-  get '/dogs/edit', to: 'dogs#edit'
-  post '/dogs/:id', to: 'dogs#update'
+  get '/dogs/:id/edit', to: 'dogs#edit'
+  patch '/dogs/:id', to: 'dogs#update'
   delete '/dogs/:id', to: 'dogs#destroy'
 end
--- spec/features/1restful_spec.rb
+++ spec/features/1restful_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "ReSTful actions" do
   it "can show all dogs" do
     samson = Dog.create!(name: "Samson", breed: "Golden Retriever", age: 14)
     phoebe = Dog.create!(name: "Phoebe", breed: "Golden Doodle", age: 11)
-    odell = Dog.create!(name: "Odell", breed: "schnoodle", age: 4)
+    odell = Dog.create!(name: "Odell", breed: "schnoodle", age: 7)
 
     visit "/dogs"
 
@@ -24,11 +24,87 @@ RSpec.describe "ReSTful actions" do
     expect(page).to have_content(odell.breed)
     expect(page).to have_content(odell.age)
   end
-  
-  it "can show a specific dog"
-  it "can show a form for a new dog"
-  it "can create a new dog"
-  it "can show a form to edit a dog"
-  it "can update a dog"
-  it "can destroy a dog"
+
+  it "can show a specific dog" do
+    samson = Dog.create!(name: "Samson", breed: "Golden Retriever", age: 14)
+
+    visit "/dogs/#{samson.id}"
+
+    expect(page).to have_content(samson.name)
+    expect(page).to have_content(samson.breed)
+    expect(page).to have_content(samson.age)
+  end
+
+  it "can show a form for a new dog" do
+    visit "/dogs/new"
+
+    expect(page).to have_field(:name)
+    expect(page).to have_field(:age)
+    expect(page).to have_field(:breed)
+    expect(page).to have_button("Create Dog")
+  end
+
+  it "can create a new dog" do
+    visit "/dogs/new"
+
+    new_name = "Samson"
+    new_age = 14
+    new_breed = "Golden Retriever"
+
+    fill_in :name, with: new_name
+    fill_in :age, with: new_age
+    fill_in :breed, with: new_breed
+
+    click_button("Create Dog")
+
+    expect(current_path).to eq("/dogs")
+    expect(page).to have_content(new_name)
+    expect(page).to have_content(new_age)
+    expect(page).to have_content(new_breed)
+  end
+
+  it "can show a form to edit a dog" do
+    samson = Dog.create(name: "Samson", age: 14, breed: "Golden Retriever")
+    visit "/dogs/#{samson.id}/edit"
+
+    expect(page).to have_content("Editing #{samson.name}")
+
+    expect(page).to have_field(:name)
+    expect(page).to have_field(:age)
+    expect(page).to have_field(:breed)
+    expect(page).to have_button("Update Dog")
+  end
+
+  it "can update a dog" do
+    samson = Dog.create(name: "Samson", age: 9, breed: "Lab")
+    visit "/dogs/#{samson.id}/edit"
+
+    new_name = "Samson"
+    new_age = 14
+    new_breed = "Golden Retriever"
+
+    fill_in :name, with: new_name
+    fill_in :age, with: new_age
+    fill_in :breed, with: new_breed
+
+    click_button("Update Dog")
+
+    expect(current_path).to eq("/dogs")
+    expect(page).to have_content(new_name)
+    expect(page).to have_content(new_age)
+    expect(page).to have_content(new_breed)
+  end
+
+  it "can destroy a dog from the dog show page" do
+    samson = Dog.create(name: "Samson", age: 9, breed: "Lab")
+
+    visit "/dogs/#{samson.id}"
+
+    click_button "delete"
+
+    expect(current_path).to eq("/dogs")
+    expect(page).to_not have_content(samson.name)
+    expect(page).to_not have_content(samson.age)
+    expect(page).to_not have_content(samson.breed)
+  end
 end
